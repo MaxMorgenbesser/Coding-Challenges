0d44900b27915628c65ef225a6ffcba68cb2db2b
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-11-25 13:03:30 -0500

    edgs case

--- odd-even-swap.js
+++ odd-even-swap.js
@@ -1,38 +1,37 @@
-// move all of ther even numbers to 
-// the beginning of the array 
+// move all of ther even numbers to
+// the beginning of the array
 // all of the odd numbers follow the even numbers
 // in O(n) time
 
+const oddeven = (arr) => {
+  let oddpointer = arr.length - 1;
+  let evenpointer = 0;
+  let num1;
+  let num2;
 
-const oddeven = (arr)=>{
-let oddpointer = arr.length-1
-let evenpointer = 0
-let num1
-let num2
+  while (oddpointer > evenpointer) {
+    // console.log(evenpointer, "-----", oddpointer);
+    if (arr[evenpointer] % 2 !== 0) {
+      num1 = arr[evenpointer];
+      num2 = arr[oddpointer];
+      arr[evenpointer] = num2;
+      arr[oddpointer] = num1;
 
-while ( oddpointer > evenpointer ){
-    
-console.log(evenpointer, '-----', oddpointer)
-if(arr[evenpointer] % 2 !== 0){
- num1 = arr[evenpointer]
- num2 = arr[oddpointer]
- arr[evenpointer] = num2
- arr[oddpointer] = num1
- 
- if (arr[evenpointer] %2 ==0){
-    evenpointer++
- }else if (arr[oddpointer]%2 != 0){
-    oddpointer--
- }
+      if (arr[evenpointer] % 2 == 0) {
+        evenpointer++;
+      } 
+      
+      if (arr[oddpointer] % 2 != 0) {
+        oddpointer--;
+      }
+    } else {
+      evenpointer++;
+    }
+   
+  }
+  return arr;
+};
 
- }else{ evenpointer++}
- console.log(arr)
+const arr = [90, 91, 30, 50, 52, 51, 99, 88, 76,88,100,1010,112,73,85];
 
-
-}
-return arr
-}
-
-const arr = [90,91,30,50,52,51,99,88,76]
-
-console.log(oddeven(arr))
+console.log(oddeven(arr));
